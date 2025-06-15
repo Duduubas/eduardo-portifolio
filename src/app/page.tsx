638bd07d0e2f017f38cd9904a04022a96635aa74
Duduubas <eduardobritogomes70@gmail.com>
2025-06-14 21:08:16 -0300

    ✨ Atualizado: estrutura da página inicial com nova mensagem e layout centralizado.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import Header from "./components/header"
-import Main from "./components/main";
-
 export default function Home() {
   return (
-    <>
-    <Header/>
-    <Main/>
-    </>
+    <main className=" flex items-center justify-center">
+      <div className="text-center p-8">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">
+          Um <span className="text-blue">novo website</span> está por vir...
+        </h1>
+        <p className="text-xl text-gray-600">
+          Estamos trabalhando em algo incrível!
+        </p>
+      </div>
+    </main>
   );
 }
